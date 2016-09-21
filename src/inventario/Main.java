cd2e404926c3343a24e836731cfc9f88a2c581a1
Noe <Noe@NOECUTZ>
2016-09-21 15:07:35 -0500

    Nada

--- src/inventario/Main.java
+++ src/inventario/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
         ventanaPrincipal.setVisible(true);
-        System.out.println("vnsajovñbaajvu");
     }
 
 }
