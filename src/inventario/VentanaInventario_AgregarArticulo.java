4771cebcdd1bb01ffbbbae283e22fddddb6937f2
Noe <Noe@NOECUTZ>
2016-09-21 19:04:46 -0500

    Agregar Articulo

--- src/inventario/VentanaInventario_AgregarArticulo.java
+++ src/inventario/VentanaInventario_AgregarArticulo.java
@@ -1,5 +1,8 @@
 package inventario;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class VentanaInventario_AgregarArticulo extends javax.swing.JFrame {
@@ -13,32 +16,28 @@ public class VentanaInventario_AgregarArticulo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        areaTextoClave = new javax.swing.JTextField();
-        areaTextoDescripcion = new javax.swing.JTextField();
-        areaTextoPrecio = new javax.swing.JTextField();
-        areaTextoCantidad = new javax.swing.JTextField();
+        campoTextoClave = new javax.swing.JTextField();
+        campoTextoDescripcion = new javax.swing.JTextField();
+        campoTextoPrecio = new javax.swing.JTextField();
+        campoTextoCantidad = new javax.swing.JTextField();
         etiquetaClave = new javax.swing.JLabel();
         etiquetaDescripcion = new javax.swing.JLabel();
         etiquetaCantidad = new javax.swing.JLabel();
-        agregarJButton = new javax.swing.JButton();
-        cancelarJButton = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        botonAgregar = new javax.swing.JButton();
+        botonCancelar = new javax.swing.JButton();
         etiquetaPrecio = new javax.swing.JLabel();
-
-        jLabel2.setText("jLabel2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Registro de Artículo");
         setResizable(false);
 
-        areaTextoClave.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        campoTextoClave.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
 
-        areaTextoDescripcion.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        campoTextoDescripcion.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
 
-        areaTextoPrecio.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        campoTextoPrecio.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
 
-        areaTextoCantidad.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        campoTextoCantidad.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
 
         etiquetaClave.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         etiquetaClave.setText("Clave");
@@ -49,19 +48,19 @@ public class VentanaInventario_AgregarArticulo extends javax.swing.JFrame {
         etiquetaCantidad.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         etiquetaCantidad.setText("Cantidad");
 
-        agregarJButton.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        agregarJButton.setText("Agregar");
-        agregarJButton.addActionListener(new java.awt.event.ActionListener() {
+        botonAgregar.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        botonAgregar.setText("Agregar");
+        botonAgregar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                agregarJButtonActionPerformed(evt);
+                botonAgregarActionPerformed(evt);
             }
         });
 
-        cancelarJButton.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        cancelarJButton.setText("Cancelar");
-        cancelarJButton.addActionListener(new java.awt.event.ActionListener() {
+        botonCancelar.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        botonCancelar.setText("Cancelar");
+        botonCancelar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cancelarJButtonActionPerformed(evt);
+                botonCancelarActionPerformed(evt);
             }
         });
 
@@ -81,24 +80,23 @@ public class VentanaInventario_AgregarArticulo extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(etiquetaDescripcion)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel1)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGap(6, 6, 6)
                                         .addComponent(etiquetaPrecio))
                                     .addComponent(etiquetaCantidad))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(areaTextoDescripcion, javax.swing.GroupLayout.DEFAULT_SIZE, 146, Short.MAX_VALUE)
-                                    .addComponent(areaTextoCantidad)
-                                    .addComponent(areaTextoPrecio)))
+                                    .addComponent(campoTextoDescripcion, javax.swing.GroupLayout.DEFAULT_SIZE, 146, Short.MAX_VALUE)
+                                    .addComponent(campoTextoCantidad)
+                                    .addComponent(campoTextoPrecio)))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(etiquetaClave)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(areaTextoClave)))
+                                .addComponent(campoTextoClave)))
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(agregarJButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(botonAgregar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(18, 18, 18)
-                        .addComponent(cancelarJButton)))
+                        .addComponent(botonCancelar)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -106,65 +104,68 @@ public class VentanaInventario_AgregarArticulo extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(areaTextoClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(campoTextoClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(etiquetaClave))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(areaTextoDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(campoTextoDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(etiquetaDescripcion))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(areaTextoCantidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(campoTextoCantidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(etiquetaCantidad))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(areaTextoPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1)
+                    .addComponent(campoTextoPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(etiquetaPrecio))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(agregarJButton)
-                    .addComponent(cancelarJButton))
+                    .addComponent(botonAgregar)
+                    .addComponent(botonCancelar))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void agregarJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregarJButtonActionPerformed
-       try{
+    private void botonAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonAgregarActionPerformed
+       
        ManejadorBD baseDeDatos = new ManejadorBD();
-        baseDeDatos.conectarConMySQL("root", "", "ElCaballoDeHierro");
-        baseDeDatos.insertarTupla("Articulos", claveJTextField.getText(),
-                descripcionJTextField.getText(),
-                Integer.parseInt(cantidadJTextField.getText()),
-                Integer.parseInt(precioJTextField.getText()));
-        baseDeDatos.desconectarConMySQL();
+        try {
+            baseDeDatos.conectarConMySQL("root", "", "ElCaballoDeHierro");
+            String valores ="\"" + campoTextoClave.getText() + "\", "
+                       + "\"" + campoTextoDescripcion.getText() + "\", "
+                       + "\"" + campoTextoCantidad.getText() + "\", "
+                       + "\"" + campoTextoPrecio.getText() + "\"";
+            baseDeDatos.insertarTupla("Articulos",valores);
+            baseDeDatos.desconectarConMySQL();
+        } catch (ClassNotFoundException excepcionBibliotecaNoHallada) {
+             JOptionPane.showMessageDialog(null, "La bilioteca MySQL_conector no hallada");
+        } catch (SQLException excepcionElementoNoHallado) {
+            JOptionPane.showMessageDialog(null, "Elemento no encontrado");
+        }
+        
+        
+       
         this.dispose();
-       }
-       catch(Exception e)
-       {
-           JOptionPane.showMessageDialog(null, "Error al almacenar el artículo");
-       }
-    }//GEN-LAST:event_agregarJButtonActionPerformed
+       
+    }//GEN-LAST:event_botonAgregarActionPerformed
 
-    private void cancelarJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelarJButtonActionPerformed
+    private void botonCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonCancelarActionPerformed
         this.dispose();
-    }//GEN-LAST:event_cancelarJButtonActionPerformed
+    }//GEN-LAST:event_botonCancelarActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton agregarJButton;
-    private javax.swing.JTextField areaTextoCantidad;
-    private javax.swing.JTextField areaTextoClave;
-    private javax.swing.JTextField areaTextoDescripcion;
-    private javax.swing.JTextField areaTextoPrecio;
-    private javax.swing.JButton cancelarJButton;
+    private javax.swing.JButton botonAgregar;
+    private javax.swing.JButton botonCancelar;
+    private javax.swing.JTextField campoTextoCantidad;
+    private javax.swing.JTextField campoTextoClave;
+    private javax.swing.JTextField campoTextoDescripcion;
+    private javax.swing.JTextField campoTextoPrecio;
     private javax.swing.JLabel etiquetaCantidad;
     private javax.swing.JLabel etiquetaClave;
     private javax.swing.JLabel etiquetaDescripcion;
     private javax.swing.JLabel etiquetaPrecio;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
